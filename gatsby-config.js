fb968ae6fe6c4d04966dec43abda33069b48429a
RayBDev <raybernard1337@gmail.com>
2018-09-05 14:51:25 -0600

    Fixed directory structure

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,10 +2,12 @@ module.exports = {
   siteMetadata: {
     title: 'Natours',
     description: 'Natours | Exciting tours for adventurous people',
-    keywords: 'natours, nature tours'
+    keywords: 'natours, nature tours',
   },
   plugins: [
-    'gatsby-plugin-react-helmet', `gatsby-plugin-sass`,
-    `gatsby-plugin-react-next`
+    'gatsby-plugin-react-helmet',
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-react-next`,
   ],
+  pathPrefix: `/natours`,
 }
